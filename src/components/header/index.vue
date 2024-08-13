d1643e1f34da9aa44c605e516ae0347b39f838e5
inno-cent <ekweozorinnocent@gmail.com>
2024-08-13 22:34:31 +0100

    files setup

---
+++ src/components/header/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header--container">
+    <header>
+        <div class="header--inner">
+            <div class="header--logo">
+                <div class="logo--wrapper">
+                    <img src="" alt="">
+                </div>
+            </div>
+        </div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
