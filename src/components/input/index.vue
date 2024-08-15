85fd023f47cac5c0cab18b218338581f556e9299
inno-cent <ekweozorinnocent@gmail.com>
2024-08-15 13:01:18 +0100

    feat:input component

---
+++ src/components/input/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="input-wrapper">
+      <label v-if="label">{{ label }}</label>
+      <div class="input-container">
+        <input :placeholder="placeholder" />
+        <div class="icon" v-if="icon">
+          <img :src="icon" alt="Icon" />
+        </div>
+      </div>
+    </div>
+  </template>
