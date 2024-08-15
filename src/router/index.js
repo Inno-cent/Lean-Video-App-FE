181415764be62b869ca2ae6c7927264e0998b20b
inno-cent <ekweozorinnocent@gmail.com>
2024-08-15 20:28:49 +0100

    setup cont

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Home from '@views/home/index.vue';
 import Login from "@/views/auth/login/index.vue"
 import Signup from "@/views/auth/signup/index.vue"
 import Forgotpassword from "@views/auth/forgot-password/index.vue"
+import ResetPassword from "../views/auth/reset-password/index.vue"
 
 const routes = [
   {
@@ -24,6 +25,11 @@ const routes = [
     path: '/forgot-password',
     name: 'Forgotpassword',
     component: Forgotpassword
+  },
+  {
+    path: '/reset-password',
+    name: 'Resetpassword',
+    component: ResetPassword
   },
 ];
 
--- src/views/auth/reset-password/index.vue
+++ src/views/auth/reset-password/index.vue
@@ -4,11 +4,14 @@
       <div class="forgot--inner--wrapper">
         <div class="wee23e">
           <div class="header--twxt">
-            <h1>Forget-password</h1>
-            <p>Please enter your email address to reset your password</p>
+            <h1>Reset your password</h1>
+            <p>Enter your new password below to reset your password.</p>
           </div>
           <div class="reset--input--wrapper">
-            <Input label="Email" placeholder="Email" />
+            <Input label="Email" placeholder="New Password" />
+          </div>
+          <div class="reset--input--wrapper">
+            <Input label="Email" placeholder="Re-enter New Password" />
           </div>
           <div class="reset--btn-wrap">
             <button class="reset--button">Send reset link</button>
@@ -16,18 +19,18 @@
         </div>
         <div class="d34rgfd2">
           <div class="acct--textq">
-           Have your password? <span> Log in</span>
+            Have your password? <span> Log in</span>
           </div>
         </div>
       </div>
     </div>
   </div>
 </template>
-
-<script>
+  
+  <script>
 import Input from "@components/input/index.vue";
 export default {
-  name: "Forgot-password",
+  name: "Resetpassword",
   data() {
     return {};
   },
@@ -37,7 +40,3 @@ export default {
   methods: {},
 };
 </script>
-
-<style scoped lang="scss">
-@import "./index.scss";
-</style>
