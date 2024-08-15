b9659553f649cb6a9849bd935a2da33947121a57
inno-cent <ekweozorinnocent@gmail.com>
2024-08-15 13:02:37 +0100

    feat:input component

--- src/components/input/index.vue
+++ src/components/input/index.vue
@@ -1,11 +1,34 @@
 <template>
-    <div class="input-wrapper">
-      <label v-if="label">{{ label }}</label>
-      <div class="input-container">
-        <input :placeholder="placeholder" />
-        <div class="icon" v-if="icon">
-          <img :src="icon" alt="Icon" />
-        </div>
+  <div class="input-wrapper">
+    <label v-if="label">{{ label }}</label>
+    <div class="input-container">
+      <input :placeholder="placeholder" />
+      <div class="icon" v-if="icon">
+        <img :src="icon" alt="Icon" />
       </div>
     </div>
-  </template>
+  </div>
+</template>
+
+  <script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+    placeholder: {
+      type: String,
+      default: "Enter text here...",
+    },
+    icon: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import "./index.scss";
+</style>/
