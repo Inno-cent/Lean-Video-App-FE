edd96c558499f3640c27be9b1e2d3392205d6f54
inno-cent <ekweozorinnocent@gmail.com>
2024-08-16 22:23:35 +0100

    about page vision

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -58,31 +58,104 @@
       </section>
 
       <section class="mission--section--container light">
-      <div class="mission--section">
-        <div class="mission--section--inner">
-          <div class="part--mission">
-            <div class="left--head--text">
-              <h4>Our Vision</h4>
-              <p>
-                We envision a world where distance is no longer an obstacle to
-                meaningful connections. At LeanVideo App, our goal is to
-                redefine digital communication by creating a platform that
-                brings people closer together, no matter where they are.<br />
-                As we continue to innovate, we aim to set a new standard in how
-                people connect, fostering relationships and collaboration that
-                transcend borders. We are committed to building a future where
-                every conversation feels as natural and vibrant as being in the
-                same room.
-              </p>
+        <div class="mission--section">
+          <div class="mission--section--inner vision">
+            <div class="part--mission">
+              <div class="left--head--text light">
+                <h4>Our Vision</h4>
+                <p>
+                  We envision a world where distance is no longer an obstacle to
+                  meaningful connections. At LeanVideo App, our goal is to
+                  redefine digital communication by creating a platform that
+                  brings people closer together, no matter where they are.<br />
+                  As we continue to innovate, we aim to set a new standard in
+                  how people connect, fostering relationships and collaboration
+                  that transcend borders. We are committed to building a future
+                  where every conversation feels as natural and vibrant as being
+                  in the same room.
+                </p>
+              </div>
+            </div>
+            <div class="part--mission">
+              <div class="mission">
+                <img src="../../assets/images/visionimage.png" alt="" />
+              </div>
             </div>
           </div>
-          <div class="part--mission">
-            <div class="mission">
-              <img src="../../assets/images/visionimage.png" alt="" />
+        </div>
+      </section>
+
+      <div class="team--container">
+        <div class="section">
+          <div class="works--conatiner">
+            <h1 class="landing--header">Meet Our Team</h1>
+            <p class="landing--subtext">
+              Discover the dedicated experts shaping the future of seamless,
+              global communication with their passion and expertise
+            </p>
+          </div>
+          <div class="we34r">
+            <div class="wetgf4f">
+              <div class="box">
+                <div class="image--wrapper">
+                  <img src="../../assets/images/team.png" alt="profile" />
+                </div>
+                <div class="box--text">
+                  <div class="main--text">
+                    <h3>Alex Johnson</h3>
+                    <p>
+                      Founder & CEO
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="box">
+                <div class="image--wrapper">
+                  <img
+                    src="../../assets//images/addcct.png"
+                    alt="create--account"
+                  />
+                </div>
+                <div class="box--text">
+                  <div class="main--text">
+                    <h3>Add Contacts</h3>
+                    <p>
+                      Quickly add contacts and start high-definition video or
+                      audio calls with a single click
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="box">
+                <div class="image--wrapper">
+                  <img
+                    src="../../assets//images/startcall.png"
+                    alt="create--account"
+                  />
+                </div>
+                <div class="box--text">
+                  <div class="main--text">
+                    <h3>Start a call</h3>
+                    <p>
+                      Click on a contact to instantly start a high-definition
+                      video or audio call with a single click
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
+
+      <section class="section">
+        <Connect />
+      </section>
+
+      <section class="section">
+        <Footer />
       </section>
     </div>
   </div>
@@ -94,7 +167,7 @@ import Header from '@components/header/index.vue';
 import Connect from '../../components/connectsection/index.vue';
 import Footer from '@components/footer/index.vue';
 export default {
-  name: 'FAQ',
+  name: 'About',
   data() {},
   components: {
     Header,
